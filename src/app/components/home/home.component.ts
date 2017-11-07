1cab38f9176a592b52cdf22107240739eff3cba1
Fergal Moran <fergal.moran@gmail.com>
2017-11-07 00:27:45 +0000

    Added new template

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,10 +11,6 @@ import { AuthService } from '../../services/auth.service';
 export class HomeComponent implements OnInit {
 
     constructor(private _rootComponent: AppComponent, private _authService: AuthService, private _router: Router) {
-        this._rootComponent.cssClass = 'app header-fixed aside-menu-fixed aside-menu-hidden pace-done sidebar-hidden';
-        // if (this._authService.authenticated()){
-        //     this._router.navigateByUrl('/podcasts');
-        // }
     }
 
     ngOnInit() {
