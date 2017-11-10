aefc02c524f854e763dae9c85950c2eb651bd59c
Fergal Moran <fergal.moran@gmail.com>
2017-11-10 22:06:10 +0000

    Added debug info

--- src/app/pipes/filter-entry.pipe.ts
+++ src/app/pipes/filter-entry.pipe.ts
@@ -1,11 +1,10 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {PodcastEntryModel} from '../models/podcasts.models';
+import { Pipe, PipeTransform } from '@angular/core';
+import { PodcastEntryModel } from '../models/podcasts.models';
 
 @Pipe({
     name: 'filterEntry',
     pure: false
 })
-
 export class FilterEntryPipe implements PipeTransform {
     transform(items: PodcastEntryModel[], ...args: any[]): PodcastEntryModel[] {
         if (items == null) {
