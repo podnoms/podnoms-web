56ffce7fb14c088580d10e88680afdc75e6231cc
Fergal Moran <fergal.moran@gmail.com>
2017-11-21 19:04:40 +0000

    Auth0 self-host trial

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './components/login/login.component';
 import { DebugComponent } from './components/debug/debug.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -7,13 +8,13 @@ import { PodcastComponent } from './components/podcast/podcast.component';
 import { PodcastAddFormComponent } from './components/podcast/podcast-add-form/podcast-add-form.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'debug', component: DebugComponent },
     { path: 'podcasts', component: PodcastComponent, canActivate: [AuthGuard] },
     { path: 'add', component: PodcastAddFormComponent, canActivate: [AuthGuard] },
     { path: 'podcasts/:slug', component: PodcastComponent, canActivate: [AuthGuard] },
     { path: 'podcasts/:slug/edit', component: PodcastAddFormComponent, canActivate: [AuthGuard] },
-    { path: 'debug', component: DebugComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
