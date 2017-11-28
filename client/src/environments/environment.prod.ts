99f1f48ab589571d85a440f4c5d5088b0889fb56
Fergal Moran <fergal.moran@gmail.com>
2017-11-28 21:14:26 +0000

    Fixed invalid port config in prod redirect URL and nginx delete snafu

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     SIGNALR_HOST: 'https://rt.podnoms.com/',
-    AUTH0_REDIRECT_URL: 'https://podnoms.com:4200/callback'
+    AUTH0_REDIRECT_URL: 'https://podnoms.com/callback'
 };
