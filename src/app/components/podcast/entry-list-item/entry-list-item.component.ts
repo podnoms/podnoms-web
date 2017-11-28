dad2c190de2ce79e4200e54ca47ee5ad9214198d
Fergal Moran <fergal.moran@gmail.com>
2017-11-28 20:24:06 +0000

    Added callback URL to environment

--- src/app/components/podcast/entry-list-item/entry-list-item.component.ts
+++ src/app/components/podcast/entry-list-item/entry-list-item.component.ts
@@ -24,7 +24,7 @@ export class EntryListItemComponent implements OnInit {
     constructor(private _signalrService: SignalRService, private _store: Store<ApplicationState>) {}
     ngOnInit() {
         if (!this.entry.processed && this.entry.processingStatus !== 'Failed') {
-            this._signalrService.init(`${environment.signalRHost}hubs/audioprocessing`);
+            this._signalrService.init(`${environment.SIGNALR_HOST}hubs/audioprocessing`);
 
             const updateChannel: string = `${this.entry.uid}__progress_update`;
             const processedChannel: string = `${this.entry.uid}__info_processed`;
