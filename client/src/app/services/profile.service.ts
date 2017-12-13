f53ca6090c46fe4ea654d83b47898bdaf251b07f
Fergal Moran <fergal.moran@gmail.com>
2017-12-13 21:03:33 +0000

    Removed proxy conf

--- client/src/app/services/profile.service.ts
+++ client/src/app/services/profile.service.ts
@@ -1,3 +1,4 @@
+import { environment } from 'environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ProfileModel } from 'app/models/profile.model';
@@ -9,19 +10,19 @@ export class ProfileService {
     constructor(private _http: AuthHttp) {}
 
     getProfile(): Observable<ProfileModel> {
-        return this._http.get('api/profile').map(res => res.json());
+        return this._http.get(environment.API_HOST + '/profile').map(res => res.json());
     }
 
     updateProfile(profile): Observable<ProfileModel> {
         console.log('ProfileService', 'updateProfile', profile);
-        return this._http.post('api/profile', profile).map(res => res.json());
+        return this._http.post(environment.API_HOST + '/profile', profile).map(res => res.json());
     }
 
     checkSlug(slug): Observable<string> {
         console.log('profile.service.ts', 'checkSlug', slug);
-        return this._http.get('api/profile/checkslug/' + slug).map(res => res.text());
+        return this._http.get(environment.API_HOST + '/profile/checkslug/' + slug).map(res => res.text());
     }
     regenerateApiKey(): Observable<string> {
-        return this._http.post('api/profile/updateapikey', null).map(res => res.text());
+        return this._http.post(environment.API_HOST + '/profile/updateapikey', null).map(res => res.text());
     }
 }
