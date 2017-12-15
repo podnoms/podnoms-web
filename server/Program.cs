dc0c574ce9fb6f19814dded396d068869b2b2426
Fergal Moran <fergal.moran@gmail.com>
2017-12-15 19:36:11 +0000

    Added SSL to Kestrel API server

--- server/Program.cs
+++ server/Program.cs
@@ -2,27 +2,26 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace PodNoms.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+namespace PodNoms.Api {
+    public class Program {
+        public static void Main (string[] args) {
+            BuildWebHost (args).Run ();
         }
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseKestrel(options =>
-                {
-                    options.Limits.MaxRequestBodySize = 1073741824;
-                })
-                .Build();
+        public static IWebHost BuildWebHost (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseStartup<Startup> ()
+            .UseKestrel (options => {
+                options.Listen (IPAddress.Any, 5000, listenOptions =>
+                    listenOptions.UseHttps ("localhost.pfx"));
+                options.Limits.MaxRequestBodySize = 1073741824;
+            })
+            .Build ();
     }
 }
