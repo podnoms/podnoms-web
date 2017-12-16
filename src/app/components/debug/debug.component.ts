cad02c222d8f205c1f2e1842c84961152f415f02
Fergal Moran <fergal.moran@gmail.com>
2017-12-16 01:48:51 +0000

    SSL dev server & publish fixes

--- src/app/components/debug/debug.component.ts
+++ src/app/components/debug/debug.component.ts
@@ -1,9 +1,9 @@
 import { Observable } from 'rxjs/Observable';
-import { environment } from 'environments/environment';
 import { SignalRService } from 'app/services/signalr.service';
 import { HubConnection } from '@aspnet/signalr-client';
 import { Component, OnInit } from '@angular/core';
 import { DebugService } from 'app/services/debug.service';
+import { environment } from 'environments/environment';
 
 @Component({
     selector: 'app-debug',
@@ -15,17 +15,25 @@ export class DebugComponent implements OnInit {
     messagesReceived: string[] = [];
 
     debugInfo$: Observable<string>;
+    apiHost = environment.API_HOST;
+    signalrHost = environment.SIGNALR_HOST;
+    pingPong = '';
 
     constructor(private _debugService: DebugService, private _signalrService: SignalRService) {}
     ngOnInit() {
-        this._signalrService.init(`${environment.SIGNALR_HOST}hubs/debug`).then(() => {
-            this._signalrService.connection.on('Send', data => {
-                console.log('DebugService', 'signalr', data);
-                this.messagesReceived.push(data);
-                this.realtimeMessage = '';
-            });
-            this.debugInfo$ = this._debugService.getDebugInfo();
-        }).catch((err) => console.error('debug.component.ts', '_signalrService.init', err));
+        this._signalrService
+            .init(`${environment.SIGNALR_HOST}hubs/debug`)
+            .then(() => {
+                this._signalrService.connection.on('Send', data => {
+                    console.log('DebugService', 'signalr', data);
+                    this.messagesReceived.push(data);
+                    this.realtimeMessage = '';
+                });
+                this.debugInfo$ = this._debugService.getDebugInfo();
+            })
+            .catch(err => console.error('debug.component.ts', '_signalrService.init', err));
+
+        this._debugService.ping().subscribe(r => (this.pingPong = r));
     }
 
     sendMessage() {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     SIGNALR_HOST: 'https://dev.podnoms.com:5000/',
     // API_HOST: 'https://podnoms.com/api',
     // SIGNALR_HOST: 'https://rt.podnoms.com/',
-    AUTH0_REDIRECT_URL: 'http://dev.podnoms.com:4200/callback'
+    AUTH0_REDIRECT_URL: 'https://dev.podnoms.com:4200/callback'
 };
