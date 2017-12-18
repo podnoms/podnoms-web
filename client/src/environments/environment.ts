6c74de3531b81a3592aae31887375c7de61672a7
Fergal Moran <fergal.moran@gmail.com>
2017-12-18 19:42:39 +0000

    Initial token refresh logic

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
     API_HOST: 'http://dev.podnoms.com:5000',
     SIGNALR_HOST: 'http://dev.podnoms.com:5000/',
-    AUTH0_REDIRECT_URL: 'http://dev.podnoms.com:4200/callback'
+    AUTH0_REDIRECT_URL: 'http://dev.podnoms.com:4200/callback',
+    BASE_URL: 'http://dev.podnoms.com:4200/'
 };
