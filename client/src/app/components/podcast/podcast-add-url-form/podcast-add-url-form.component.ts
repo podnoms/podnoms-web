f0116d99f21fd63bc88858fe7a0f1a44b9d0bab7
Fergal Moran <fergal.moran@gmail.com>
2018-02-12 23:56:45 +0000

    Added some error handling for add entry

--- client/src/app/components/podcast/podcast-add-url-form/podcast-add-url-form.component.ts
+++ client/src/app/components/podcast/podcast-add-url-form/podcast-add-url-form.component.ts
@@ -1,20 +1,66 @@
 import { PodcastModel, PodcastEntryModel } from 'app/models/podcasts.models';
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import {
+    Component,
+    Input,
+    EventEmitter,
+    Output,
+    AfterViewInit,
+    ViewChild
+} from '@angular/core';
+import { PodcastService } from 'app/services/podcast.service';
 
 @Component({
     selector: 'app-podcast-add-url-form',
     templateUrl: './podcast-add-url-form.component.html',
     styleUrls: ['./podcast-add-url-form.component.css']
 })
-export class PodcastAddUrlFormComponent {
+export class PodcastAddUrlFormComponent implements AfterViewInit {
     @Input() podcast: PodcastModel;
     @Output() onUrlAddComplete: EventEmitter<any> = new EventEmitter();
     newEntrySourceUrl: string;
-
-    constructor() {}
-
+    errorText: string;
+    isPosting: boolean = false;
+    @ViewChild('input') vc: any;
+    constructor(private _service: PodcastService) {}
+    ngAfterViewInit() {
+        this.vc.nativeElement.focus();
+    }
+    isValidURL(str) {
+        var a = document.createElement('a');
+        a.href = str;
+        return a.host && a.host != window.location.host;
+    }
     addEntry(podcast: PodcastModel) {
-        const entry = new PodcastEntryModel(this.podcast.id, this.newEntrySourceUrl);
-        this.onUrlAddComplete.emit(entry);
+        const urlToCheck = this.newEntrySourceUrl;
+        this.newEntrySourceUrl = 'Checking (please wait).....';
+        this.errorText = '';
+        if (this.isValidURL(urlToCheck)) {
+            this.isPosting = true;
+            this._service.checkEntry(urlToCheck).subscribe(
+                r => {
+                    if (r) {
+                        const entry = new PodcastEntryModel(
+                            this.podcast.id,
+                            urlToCheck
+                        );
+                        this.onUrlAddComplete.emit(entry);
+                        this.isPosting = false;
+                        this.newEntrySourceUrl = urlToCheck;
+                    } else {
+                        this.errorText = 'This is not a supported URL';
+                        this.isPosting = false;
+                        this.newEntrySourceUrl = urlToCheck;
+                    }
+                },
+                err => {
+                    this.errorText = 'This is not a supported URL';
+                    this.isPosting = false;
+                    this.newEntrySourceUrl = urlToCheck;
+                }
+            );
+        } else {
+            this.errorText = 'This does not look like a valid URL';
+            this.newEntrySourceUrl = urlToCheck;
+        }
     }
 }
--- server/Services/Processor/IUrlProcessService.cs
+++ server/Services/Processor/IUrlProcessService.cs
@@ -1,10 +1,9 @@
 using System.Threading.Tasks;
 
-namespace PodNoms.Api.Services.Processor
-{
-    public interface IUrlProcessService
-    {
-        Task<bool> GetInformation(int entryId);
-        Task<bool> DownloadAudio(int entryId);
+namespace PodNoms.Api.Services.Processor {
+    public interface IUrlProcessService {
+        Task<bool> CheckUrlValid (string url);
+        Task<bool> GetInformation (int entryId);
+        Task<bool> DownloadAudio (int entryId);
     }
 }
