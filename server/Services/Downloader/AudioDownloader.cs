d19f94404f12060b51cd99da0712f434e4d096c1
Fergal Moran <fergal.moran@gmail.com>
2018-03-06 20:12:09 +0000

    IsValid switched to nyt

--- server/Services/Downloader/AudioDownloader.cs
+++ server/Services/Downloader/AudioDownloader.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using NYoutubeDL;
+using NYoutubeDL.Models;
 using PodNoms.Api.Models.ViewModels;
 using PodNoms.Api.Utils.Extensions;
 
@@ -20,131 +22,120 @@ namespace PodNoms.Api.Services.Downloader {
 
         public event EventHandler<ProcessProgressEvent> DownloadProgress;
         public event EventHandler<String> PostProcessing;
-        public AudioDownloader (string url, string downloader) {
+        public AudioDownloader(string url, string downloader) {
             this._url = url;
             this._downloader = downloader;
         }
 
-        public static string GetVersion (string downloader) {
+        public static string GetVersion(string downloader) {
             try {
                 var proc = new Process {
                     StartInfo = new ProcessStartInfo {
-                    FileName = downloader,
-                    Arguments = $"--version",
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
+                        FileName = downloader,
+                        Arguments = $"--version",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
                     }
                 };
-                var br = new StringBuilder ();
-                proc.Start ();
+                var br = new StringBuilder();
+                proc.Start();
                 while (!proc.StandardOutput.EndOfStream) {
-                    br.Append (proc.StandardOutput.ReadLine ());
+                    br.Append(proc.StandardOutput.ReadLine());
                 }
-                return br.ToString ();
+                return br.ToString();
             } catch (Exception ex) {
                 return $"{{\"Error\": \"{ex.Message}\"}}";
             }
         }
-        public async Task<bool> CheckUrlValid () {
-            var proc = new Process {
-                StartInfo = new ProcessStartInfo {
-                FileName = this._downloader,
-                Arguments = $"\"{this._url}\" -j",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
-                }
-            };
-            proc.EnableRaisingEvents = true;
-            proc.Exited += ((e, s) => {
-                // Console.WriteLine ($"{e.ToString()} - {s.ToString()}");
+        public async Task<bool> CheckUrlValid() {
+            var ret = false;
+            await Task.Run(() => {
+                var youtubeDl = new YoutubeDL();
+                youtubeDl.VideoUrl = this._url;
+                var info = youtubeDl.GetDownloadInfo();
+                ret = (
+                   info != null &&
+                   info is VideoDownloadInfo && //make sure it's not a playlist                                                                                                 
+                   (info.Errors.Count == 0 || info.VideoSize != null));
             });
-            proc.OutputDataReceived += ((e, s) => {
-                Console.WriteLine ($"{e.ToString()} - {s.ToString()}");
-            });
-            proc.ErrorDataReceived += ((e, s) => {
-                Console.WriteLine ($"{e.ToString()} - {s.ToString()}");
-            });
-            proc.Start ();
-            await proc.WaitForExitAsync ();
-            return (proc.ExitCode == 0);
+            return ret;
         }
 
-        public string DownloadAudio (string uid) {
-            var outputFile = Path.Combine (Path.GetTempPath (), $"{uid}.mp3");
-            var templateFile = Path.Combine (Path.GetTempPath (), $"{uid}.%(ext)s");
+        public string DownloadAudio(string uid) {
+            var outputFile = Path.Combine(Path.GetTempPath(), $"{uid}.mp3");
+            var templateFile = Path.Combine(Path.GetTempPath(), $"{uid}.%(ext)s");
             var proc = new Process {
                 StartInfo = new ProcessStartInfo {
-                FileName = this._downloader,
-                Arguments = $"-o \"{templateFile}\" --audio-format mp3 -x \"{this._url}\"",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
+                    FileName = this._downloader,
+                    Arguments = $"-o \"{templateFile}\" --audio-format mp3 -x \"{this._url}\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
                 }
             };
 
-            StringBuilder br = new StringBuilder ();
-            proc.Start ();
+            StringBuilder br = new StringBuilder();
+            proc.Start();
             while (!proc.StandardOutput.EndOfStream) {
-                string output = proc.StandardOutput.ReadLine ();
-                if (output.Contains ("%")) {
-                    var progress = _parseProgress (output);
+                string output = proc.StandardOutput.ReadLine();
+                if (output.Contains("%")) {
+                    var progress = _parseProgress(output);
                     if (DownloadProgress != null) {
-                        DownloadProgress (this, progress);
+                        DownloadProgress(this, progress);
                     }
                 } else {
                     if (PostProcessing != null) {
-                        PostProcessing (this, output);
+                        PostProcessing(this, output);
                     }
                 }
             }
 
-            if (File.Exists (outputFile)) {
+            if (File.Exists(outputFile)) {
                 return outputFile;
             }
             return string.Empty;
         }
 
-        public void DownloadInfo () {
+        public void DownloadInfo() {
             var proc = new Process {
                 StartInfo = new ProcessStartInfo {
-                FileName = "youtube-dl",
-                Arguments = $"-j {this._url}",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
+                    FileName = "youtube-dl",
+                    Arguments = $"-j {this._url}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
                 }
             };
 
-            StringBuilder br = new StringBuilder ();
-            proc.Start ();
+            StringBuilder br = new StringBuilder();
+            proc.Start();
             while (!proc.StandardOutput.EndOfStream) {
-                br.Append (proc.StandardOutput.ReadLine ());
+                br.Append(proc.StandardOutput.ReadLine());
             }
-            Properties = JsonConvert.DeserializeObject<ExpandoObject> (br.ToString ());
+            Properties = JsonConvert.DeserializeObject<ExpandoObject>(br.ToString());
         }
 
-        private ProcessProgressEvent _parseProgress (string output) {
+        private ProcessProgressEvent _parseProgress(string output) {
 
-            var result = new ProcessProgressEvent ();
+            var result = new ProcessProgressEvent();
 
-            int progressIndex = output.LastIndexOf (' ', output.IndexOf ('%')) + 1;
-            string progressString = output.Substring (progressIndex, output.IndexOf ('%') - progressIndex);
-            result.Percentage = (int) Math.Round (double.Parse (progressString));
+            int progressIndex = output.LastIndexOf(' ', output.IndexOf('%')) + 1;
+            string progressString = output.Substring(progressIndex, output.IndexOf('%') - progressIndex);
+            result.Percentage = (int)Math.Round(double.Parse(progressString));
 
-            int sizeIndex = output.LastIndexOf (' ', output.IndexOf (DOWNLOADSIZESTRING)) + 1;
-            string sizeString = output.Substring (sizeIndex, output.IndexOf (DOWNLOADSIZESTRING) - sizeIndex + 2);
+            int sizeIndex = output.LastIndexOf(' ', output.IndexOf(DOWNLOADSIZESTRING)) + 1;
+            string sizeString = output.Substring(sizeIndex, output.IndexOf(DOWNLOADSIZESTRING) - sizeIndex + 2);
             result.TotalSize = sizeString;
 
-            if (output.Contains (DOWNLOADRATESTRING)) {
-                int rateIndex = output.LastIndexOf (' ', output.LastIndexOf (DOWNLOADRATESTRING)) + 1;
-                string rateString = output.Substring (rateIndex, output.LastIndexOf (DOWNLOADRATESTRING) - rateIndex + 4);
+            if (output.Contains(DOWNLOADRATESTRING)) {
+                int rateIndex = output.LastIndexOf(' ', output.LastIndexOf(DOWNLOADRATESTRING)) + 1;
+                string rateString = output.Substring(rateIndex, output.LastIndexOf(DOWNLOADRATESTRING) - rateIndex + 4);
                 result.CurrentSpeed = rateString;
             }
 
-            if (output.Contains (ETASTRING)) {
-                result.ETA = output.Substring (output.LastIndexOf (' ') + 1);
+            if (output.Contains(ETASTRING)) {
+                result.ETA = output.Substring(output.LastIndexOf(' ') + 1);
             }
             return result;
         }
