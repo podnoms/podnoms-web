d48c18f661d60d58102dfe5b2409015e42a1be0e
Fergal Moran <fergal.moran@gmail.com>
2018-03-07 21:16:56 +0000

    ytdl updates, and processing feedback

--- server/Services/Processor/IUrlProcessService.cs
+++ server/Services/Processor/IUrlProcessService.cs
@@ -1,9 +1,10 @@
 using System.Threading.Tasks;
+using PodNoms.Api.Models;
 
 namespace PodNoms.Api.Services.Processor {
     public interface IUrlProcessService {
-        Task<bool> CheckUrlValid (string url);
         Task<bool> GetInformation (int entryId);
+        Task<bool> GetInformation (PodcastEntry entry);
         Task<bool> DownloadAudio (int entryId);
     }
 }
