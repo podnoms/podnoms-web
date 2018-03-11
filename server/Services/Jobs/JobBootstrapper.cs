5a3b3a8737332604d41714c356a80dedb7b9e1f9
Fergal Moran <fergal.moran@gmail.com>
2018-03-11 17:11:19 +0000

    Fixed job bootstrapper

--- server/Services/Jobs/JobBootstrapper.cs
+++ server/Services/Jobs/JobBootstrapper.cs
@@ -7,7 +7,7 @@ using PodNoms.Api.Persistence;
 namespace PodNoms.Api.Services.Jobs {
     public static class JobBootstrapper {
         public static void BootstrapJobs() {
-            BackgroundJob.Enqueue<ClearOrphanAudioJob>(x => x.ClearOrphanAudio());
+            RecurringJob.AddOrUpdate<ClearOrphanAudioJob>(x => x.Execute(), Cron.Daily(1));
         }
     }
 }
