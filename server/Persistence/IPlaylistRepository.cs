1a92ad80ebb925e8942c519bc788c8d20288c38d
Fergal Moran <fergal.moran@gmail.com>
2018-03-14 10:50:57 +0000

    Feature switch

--- server/Persistence/IPlaylistRepository.cs
+++ server/Persistence/IPlaylistRepository.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using PodNoms.Api.Models;
 
 namespace PodNoms.Api.Persistence {
     public interface IPlaylistRepository {
         Task<Playlist> GetAsync(int id);
+        Task<IEnumerable<Playlist>> GetAllAsync();
         Task<Playlist> AddOrUpdateAsync(Playlist playlist);
     }
 }
