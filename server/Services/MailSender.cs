c09dbfde55238376be8e2055108d6ceb4834b6fa
Fergal Moran <fergal.moran@gmail.com>
2018-03-16 17:30:56 +0000

    Mailgun API implemented

---
+++ server/Services/MailSender.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PodNoms.Api.Models;
+using System.Net;
+
+namespace PodNoms.Api.Services {
+
+    public interface IMailSender {
+        Task<bool> SendEmail(string email, string subject, string message);
+    } 
+    public class MailSender : IMailSender {
+        private readonly EmailSettings _emailSettings;
+        private readonly ILogger _logger;
+        public MailSender(IOptions<EmailSettings> emailSettings, ILogger<MailSender> logger){
+            _emailSettings = emailSettings.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEmail(string email, string subject, string message){
+            using (var client = new HttpClient { BaseAddress = new Uri(_emailSettings.ApiBaseUri) }) {
+                client.DefaultRequestHeaders.Authorization = 
+                    new AuthenticationHeaderValue("Basic",
+                            Convert.ToBase64String(Encoding.ASCII.GetBytes(_emailSettings.ApiKey)));
+                
+                _logger.LogInformation($"From: {_emailSettings.From}\nTo: {email}\nApi key: {_emailSettings.ApiKey}");
+
+                var content = new FormUrlEncodedContent(new[]
+                        {
+                        new KeyValuePair<string, string>("from", _emailSettings.From),
+                        new KeyValuePair<string, string>("to", email),
+                        new KeyValuePair<string, string>("subject", subject),
+                        new KeyValuePair<string, string>("html", message)
+                        });
+
+                var result = await client.PostAsync(_emailSettings.RequestUri, content).ConfigureAwait(false);
+                if (result.StatusCode == HttpStatusCode.OK)
+                    return true;
+                
+                _logger.LogError($"Error {result.StatusCode} sending mail\n{result.ReasonPhrase}");
+                return false;
+            }
+        }
+    }
+}
