86e46c5de0b6391b27572c43c82510b5c16f2612
Fergal Moran <fergal.moran@gmail.com>
2018-03-16 17:40:54 +0000

    Added job & emails

--- server/Services/Jobs/ClearOrphanAudioJob.cs
+++ server/Services/Jobs/ClearOrphanAudioJob.cs
@@ -13,9 +13,11 @@ namespace PodNoms.Api.Services.Jobs {
         public readonly StorageSettings _storageSettings;
         public readonly AudioFileStorageSettings _audioStorageSettings;
         private readonly ILogger<ClearOrphanAudioJob> _logger;
+        private readonly IMailSender _mailSender;
 
         public ClearOrphanAudioJob(IEntryRepository entryRepository, IOptions<StorageSettings> storageSettings,
-            IOptions<AudioFileStorageSettings> audioStorageSettings, ILoggerFactory logger) {
+            IOptions<AudioFileStorageSettings> audioStorageSettings, ILoggerFactory logger, IMailSender mailSender) {
+            this._mailSender = mailSender;
             this._storageSettings = storageSettings.Value;
             this._audioStorageSettings = audioStorageSettings.Value;
             this._entryRepository = entryRepository;
@@ -28,7 +30,7 @@ namespace PodNoms.Api.Services.Jobs {
                 CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_storageSettings.ConnectionString);
                 CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
                 CloudBlobContainer container = blobClient.GetContainerReference(_audioStorageSettings.ContainerName);
-
+                short blobCount = 0;
                 var blobs = await container.ListBlobsSegmentedAsync(null);
                 foreach (CloudBlockBlob blob in blobs.Results) {
                     try {
@@ -38,12 +40,14 @@ namespace PodNoms.Api.Services.Jobs {
                             var entry = await _entryRepository.GetByUidAsync(guid);
                             if (entry == null) {
                                 await blob.DeleteIfExistsAsync();
+                                blobCount++;
                             }
                         }
                     } catch (Exception e) {
                         _logger.LogWarning($"Error processing blob {blob.Uri}\n{e.Message}");
                     }
                 }
+                await this._mailSender.SendEmail("fergal.moran@gmail.com", $"ClearOrphanAudioJob: Complete {blobCount}", string.Empty);
             } catch (Exception ex) {
                 _logger.LogError($"Error clearing orphans\n{ex.Message}");
             }
