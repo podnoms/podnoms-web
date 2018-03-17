00c258a2949fde502b432c12f6f6a5458c7792ae
Fergal Moran <fergal.moran@gmail.com>
2018-03-17 04:10:22 +0000

    Chatter infrastructure ready

--- server/Models/ViewModels/ProfileViewModel.cs
+++ server/Models/ViewModels/ProfileViewModel.cs
@@ -1,13 +1,12 @@
-﻿namespace PodNoms.Api.Models.ViewModels
-{
-    public class ProfileViewModel
-    {
+﻿namespace PodNoms.Api.Models.ViewModels {
+    public class ProfileViewModel {
         public int Id { get; set; }
         public string Slug { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string ProfileImage { get; set; }
+        public string Uid { get; set; }
         public string ApiKey { get; set; }
     }
 }
--- server/Persistence/UserRepository.cs
+++ server/Persistence/UserRepository.cs
@@ -10,70 +10,78 @@ namespace PodNoms.Api.Persistence {
     public class UserRepository : IUserRepository {
         private readonly PodnomsDbContext _context;
 
-        public UserRepository (PodnomsDbContext context) {
+        public UserRepository(PodnomsDbContext context) {
             _context = context;
         }
 
-        public User Get (int id) {
-            return _context.Users.FirstOrDefault (u => u.Id == id);
+        public User Get(int id) {
+            return _context.Users.FirstOrDefault(u => u.Id == id);
         }
 
-        public User Get (string email) {
-            return _context.Users.FirstOrDefault (u => u.EmailAddress == email);
+        public User Get(string email) {
+            return _context.Users.FirstOrDefault(u => u.EmailAddress == email);
         }
-        public async Task<User> GetBySlugAsync (string slug) {
+        public async Task<User> GetAsync(string email) {
+            return await _context.Users
+                .Where(u => u.EmailAddress == email)
+                .FirstOrDefaultAsync();     
+        }
+        public async Task<User> GetBySlugAsync(string slug) {
             var user = await _context.Users
-                .Where (u => u.Slug == slug)
-                .FirstOrDefaultAsync ();
+                .Where(u => u.Slug == slug)
+                .FirstOrDefaultAsync();
 
             return user;
         }
 
-        public User AddOrUpdate (User user) {
+        public User AddOrUpdate(User user) {
             if (user.Id != 0) {
-                _context.Users.Attach (user);
+                _context.Users.Attach(user);
             } else {
-                _context.Users.Add (user);
+                _context.Users.Add(user);
 
             }
             return user;
         }
 
-        public User UpdateRegistration (string email, string name, string sid, string providerId, string profileImage, string refreshToken) {
-            var user = _context.Users.FirstOrDefault (u => u.EmailAddress == email);
+        public User UpdateRegistration(string email, string name, string sid, string providerId, string profileImage,
+                                             string refreshToken) {
+            var user = _context.Users.FirstOrDefault(u => u.EmailAddress == email);
 
             if (user == null) {
-                user = new User ();
-                user = new User ();
+                user = new User();
                 user.EmailAddress = email;
             }
-            user.FullName = string.IsNullOrEmpty (user.FullName) ? name : user.FullName;
-            if (string.IsNullOrEmpty (user.Slug)) {
-                var c = user.FullName ?? email?.Split ('@') [0] ?? string.Empty;
-                if (!string.IsNullOrEmpty (c)) {
-                    user.Slug = c.Slugify (
+            user.FullName = string.IsNullOrEmpty(user.FullName) ? name : user.FullName;
+            if (string.IsNullOrEmpty(user.Slug)) {
+                var c = user.FullName ?? email?.Split('@')[0] ?? string.Empty;
+                if (!string.IsNullOrEmpty(c)) {
+                    user.Slug = c.Slugify(
                         from u in _context.Users select u.Slug);
                 }
             }
-            if (!string.IsNullOrEmpty (refreshToken)) {
+            if (string.IsNullOrEmpty(user.Uid)) {
+                user.Uid = System.Guid.NewGuid().ToString();
+            }
+            if (!string.IsNullOrEmpty(refreshToken)) {
                 user.RefreshToken = refreshToken;
             }
             user.Sid = sid;
             user.ProfileImage = profileImage;
 
-            if (string.IsNullOrEmpty (user.ApiKey))
-                UpdateApiKey (user);
+            if (string.IsNullOrEmpty(user.ApiKey))
+                UpdateApiKey(user);
 
-            AddOrUpdate (user);
+            AddOrUpdate(user);
             return user;
         }
 
-        public string UpdateApiKey (User user) {
+        public string UpdateApiKey(User user) {
             var newKey = "";
             if (user != null) {
                 do {
-                    newKey = Randomisers.RandomString (16);
-                } while (_context.Users.FirstOrDefault (u => u.ApiKey == newKey) != null);
+                    newKey = Randomisers.RandomString(16);
+                } while (_context.Users.FirstOrDefault(u => u.ApiKey == newKey) != null);
             }
             user.ApiKey = newKey;
             return newKey;
