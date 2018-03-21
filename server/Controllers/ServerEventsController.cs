a146aecdfac972b58922f479cb1c1da1bc1bf1cb
Fergal Moran <fergal.moran@gmail.com>
2018-03-21 11:32:40 +0000

    Staging changes

---
+++ server/Controllers/ServerEventsController.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PodNoms.Api.Controllers {
+    [Route("/sse")]
+    public class ServerEventsController : Controller {
+        public async Task<
+    }
+}
