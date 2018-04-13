7ce29ab6e6150f7d5b7f54af8656eed45ab3e6c7
Fergal Moran <fergal.moran@gmail.com>
2018-04-13 22:03:05 +0100

    Restore permissions chmod change (ignore)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,11 @@
+
+const ROOT_URL = 'https://api.podnoms.com';
+
 export const environment = {
     production: false,
-    API_HOST: 'http://localhost:5000',
-    SIGNALR_HOST: 'http://localhost:5000/',
+    // API_HOST: 'http://localhost:5000',
+    API_HOST: ROOT_URL,
+    SIGNALR_HOST: 'https://api.podnoms.com',
     AUTH0_REDIRECT_URL: 'http://localhost:4200/callback',
     BASE_URL: 'http://localhost:4200/',
     appInsights: {
