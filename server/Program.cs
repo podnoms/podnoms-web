66b10579c7d82d5b38d1c99c02d7432c1177462b
Fergal Moran <fergal.moran@gmail.com>
2018-04-21 07:29:57 +0100

    Increase HTTP MaxBodySize (for file uploads)

--- server/Program.cs
+++ server/Program.cs
@@ -9,25 +9,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace PodNoms.Api {
-    public class Program {
+namespace PodNoms.Api
+{
+    public class Program
+    {
         static bool isDevelopment =
             Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == EnvironmentName.Development;
 
-        public static void Main(string[] args) {
+        public static void Main(string[] args)
+        {
             CreateWebHostBuilder(args).Build().Run();
         }
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
             .UseStartup<Startup>()
-            .UseUrls("http://0.0.0.0:5000");
-        // .UseKestrel(options => {
-        //         options.Listen(IPAddress.Any, 5000);
-        //         if (isDevelopment){
-        //             options.Listen(IPAddress.Any, 5001, listenOptions => {
-        //                 listenOptions.UseHttps("certs/dev2.podnoms.com.pfx", "secret");
-        //             });
-        //         }
-        // });
+            .UseUrls("http://0.0.0.0:5000")
+             .UseKestrel(options =>
+             {
+                 options.Limits.MaxRequestBodySize = 1073741824; //1Gb
+                                                                 //  if (isDevelopment)
+                                                                 //  {
+                                                                 //      options.Listen(IPAddress.Any, 5001, listenOptions =>
+                                                                 //      {
+                                                                 //          listenOptions.UseHttps("certs/dev2.podnoms.com.pfx", "secret");
+                                                                 //      });
+                                                                 //  }
+             });
     }
 }
