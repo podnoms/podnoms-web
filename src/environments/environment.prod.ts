882e1bc68b0aa077ff73494b39b45b3368169070
Fergal Moran <fergal.moran@gmail.com>
2018-04-25 18:50:29 +0100

    Removed outdated user repo stuff

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,9 @@
 export const environment = {
     production: true,
-    // API_HOST: 'https://api.podnoms.com',
     API_HOST: 'https://api.podnoms.com',
     SIGNALR_HOST: 'https://rt.podnoms.com',
-    AUTH0_REDIRECT_URL: 'https://podnoms.com/callback',
     BASE_URL: 'https://podnoms.com',
     appInsights: {
         instrumentationKey: '020b002a-bd3d-4b25-8a74-cab16fd39dfc'
     }
 };
-
