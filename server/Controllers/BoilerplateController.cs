9d1914a2d8776471daec56184ca17b0a1694651b
Fergal Moran <fergal.moran@gmail.com>
2018-05-03 07:36:51 +0100

    Fix swagger endpoint

--- server/Controllers/BoilerplateController.cs
+++ server/Controllers/BoilerplateController.cs
@@ -5,6 +5,7 @@ using PodNoms.Api.Utils;
 namespace PodNoms.Api.Controllers {
     [Route("[controller]")]
     public class BoilerplateController : Controller {
+        [HttpGet]
         public async Task<ActionResult<string>> Get(string key) {
             var html = await ResourceReader.ReadResource($"{key}.html");
             return Content(html, "text/plain");
