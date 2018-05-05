91640f53ffed116b833c0d1a6f73d4e12f2fb002
Fergal Moran <fergal.moran@gmail.com>
2018-05-05 02:20:26 +0100

    Fixed invalid login error messages

--- client/src/app/services/base.service.ts
+++ client/src/app/services/base.service.ts
@@ -3,15 +3,25 @@ import { Observable } from 'rxjs/Rx';
 export abstract class BaseService {
     constructor() {}
 
-    protected handleError(error: any) {
-        const applicationError = error.headers.get('Application-Error');
+    protected handleError(response: any) {
+        const applicationError = response.headers.get('Application-Error');
 
         // either applicationError in header or model error in body
         if (applicationError) {
             return Observable.throw(applicationError);
         }
 
-        const serverError = error.error.errors[0].description;
-        return Observable.throw(serverError || 'Server error');
+        let modelStateErrors: string = '';
+        const serverError = response.error; //json();
+
+        if (!serverError.type) {
+            for (let key in serverError) {
+                if (serverError[key])
+                    modelStateErrors += serverError[key] + '\n';
+            }
+        }
+
+        modelStateErrors = modelStateErrors = '' ? null : modelStateErrors;
+        return Observable.throw(modelStateErrors || 'Server error');
     }
 }
