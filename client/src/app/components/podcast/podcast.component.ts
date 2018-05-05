3ea6e3bac2a85df0c807e471b77804eb9107b4d3
Fergal Moran <fergal.moran@gmail.com>
2018-05-05 02:58:07 +0100

    Added cookie consent

--- client/src/app/components/podcast/podcast.component.ts
+++ client/src/app/components/podcast/podcast.component.ts
@@ -15,6 +15,7 @@ import { UpdateAction, AddAction } from 'app/actions/entries.actions';
 import * as fromPodcast from 'app/reducers';
 import * as fromPodcastActions from 'app/actions/podcast.actions';
 import * as fromEntriesActions from 'app/actions/entries.actions';
+import { NgcCookieConsentService } from 'ngx-cookieconsent';
 
 @Component({
     selector: 'app-podcast',
@@ -41,20 +42,21 @@ export class PodcastComponent {
         private _store: Store<ApplicationState>,
         private _service: PodcastService,
         private _toasty: ToastyService,
+        private _cookieConsentService: NgcCookieConsentService,
         route: ActivatedRoute,
         private _location: Location
     ) {
         this.selectedPodcast$ = _store.select(fromPodcast.getSelectedPodcast);
 
         this.entries$ = _store.select(fromPodcast.getEntries);
-        route.params.subscribe(params => {
+        route.params.subscribe((params) => {
             let slug = params['slug'];
             if (slug !== undefined) {
                 this.firstRun = false;
                 _store.dispatch(new fromEntriesActions.LoadAction(slug));
                 _store.dispatch(new fromPodcastActions.SelectAction(slug));
             } else {
-                this.selectedPodcast$.subscribe(r => {
+                this.selectedPodcast$.subscribe((r) => {
                     if (r) {
                         slug = r.slug;
                         _store.dispatch(
@@ -102,7 +104,7 @@ export class PodcastComponent {
     }
     processPlaylist() {
         if (this.pendingEntry) {
-            this._service.addPlaylist(this.pendingEntry).subscribe(e => {
+            this._service.addPlaylist(this.pendingEntry).subscribe((e) => {
                 if (e) {
                     this._toasty.info(
                         'Playlist added, check back here (and on your device) for new episodes'
