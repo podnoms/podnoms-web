cf5c7d43a7bc062adf5861c32083424dca519891
Fergal Moran <fergal.moran@gmail.com>
2018-05-06 23:23:23 +0100

    Waiting for token reset

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,11 @@ export const environment = {
     API_HOST: 'https://api.podnoms.com',
     SIGNALR_HOST: 'https://rt.podnoms.com',
     BASE_URL: 'https://podnoms.com',
+    HELP_URL: 'https://talk.podnoms.com',
     appInsights: {
         instrumentationKey: '020b002a-bd3d-4b25-8a74-cab16fd39dfc'
+    },
+    messaging: {
+        endpoint: 'https://fcm.googleapis.com/fcm/send'
     }
 };
