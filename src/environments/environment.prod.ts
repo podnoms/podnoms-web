67d14e71506b1e978cfa36e2c64f2c8f295c05c3
Fergal Moran <fergal.moran@gmail.com>
2018-05-29 00:50:36 +0100

    Added missing prod build stuff

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
     production: true,
-    apiHost: 'https://api.podnoms.com'
+    apiHost: 'https://api.podnoms.com',
+    signalRHost: 'https://rt.podnoms.com/',
+    helpUrl: 'https://talk.podnoms.com/',
+    appInsights: {
+        instrumentationKey: '020b002a-bd3d-4b25-8a74-cab16fd39dfc'
+    }
 };
