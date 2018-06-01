a7df75bfecad00a75381621196fc2cb2265b3089
Fergal Moran <fergal.moran@gmail.com>
2018-06-01 21:56:20 +0100

    require versions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
     apiHost: 'https://api.podnoms.com',
     signalRHost: 'https://rt.podnoms.com/',
     helpUrl: 'https://talk.podnoms.com/',
-    version: '0.30.0',
+    version: require('../../package.json').version,
     appInsights: {
         instrumentationKey: '020b002a-bd3d-4b25-8a74-cab16fd39dfc'
     }
