ca1c0d0303f73b3bc8e6ca926bb002be3d6ec1a2
Fergal Moran <fergal.moran@gmail.com>
2018-06-22 17:02:06 +0100

    Pre ngsw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ComponentsModule } from './components/components.module';
 import { SimpleNotificationsModule } from 'angular2-notifications';
 import { SharedModule } from './shared/shared.module';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 @NgModule({
     imports: [
         BrowserModule,
@@ -21,7 +22,8 @@ import { environment } from '../environments/environment';
         AppRoutingModule,
         AppStoreModule,
         SharedModule, // import here to make sure that AuthService is a singleton
-        SimpleNotificationsModule.forRoot()
+        SimpleNotificationsModule.forRoot(),
+        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
     ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
