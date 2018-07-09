a468391dcd2d9bbd4daf177831652523f581c53a
Fergal Moran <fergal.moran@gmail.com>
2018-07-09 21:34:48 +0100

    Fix debug api url in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
 export const environment = {
     production: true,
-    // apiHost: 'https://api.podnoms.com',
-    apiHost: 'http://localhost:5000',
-    // signalRHost: 'https://rt.podnoms.com/',
-    signalRHost: 'http://localhost:5000',
+    apiHost: 'https://api.podnoms.com',
+    signalRHost: 'https://rt.podnoms.com/',
     vapidPublicKey:
         'BJQY5jNSGoa3SVqxlHH3fyhpBx_7pMrqijh92bM4cwZlmfSYrsRG-8Ci1VYkHr3W13Uh2nWmLTRL00pc7HBdias',
     helpUrl: 'https://talk.podnoms.com/',
