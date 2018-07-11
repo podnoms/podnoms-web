2284a101b080f86e4f0d86355babcd315d54506e
Fergal Moran <fergal.moran@gmail.com>
2018-07-11 19:35:32 +0100

    Added some initial debug/job scheduler stuff

--- src/app/shared/components/debug/debug.component.ts
+++ src/app/shared/components/debug/debug.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DebugService } from '../../services/debug.service';
+import { JobService } from '../../services/job.service';
 
 @Component({
     selector: 'app-debug',
@@ -7,10 +8,13 @@ import { DebugService } from '../../services/debug.service';
     styleUrls: ['./debug.component.scss']
 })
 export class DebugComponent implements OnInit {
-    constructor(private debugService: DebugService) {}
+    constructor(private debugService: DebugService, private jobService: JobService) {}
 
     ngOnInit() {}
     sendPush() {
         this.debugService.sendPushPessage('Hello Sailor').subscribe(r => alert(r));
+    }
+    processMissing() {
+        this.jobService.processMissing().subscribe(r => alert(r));
     }
 }
