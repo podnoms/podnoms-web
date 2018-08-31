8e37ad8257bf50f0da20bdafa5c5274280918de0
Fergal Moran <fergal.moran@gmail.com>
2018-08-31 23:54:48 +0100

    Unfuck package upgrades

--- src/app/podcasts/entries-store.service.ts
+++ src/app/podcasts/entries-store.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { EntityCollectionServiceBase, EntityCollectionServiceFactory } from 'ngrx-data';
+import { EntityCollectionServiceBase, EntityCollectionServiceElementsFactory } from 'ngrx-data';
 import { PodcastEntry } from '../core';
 
 @Injectable({
     providedIn: 'root'
 })
 export class EntriesStoreService extends EntityCollectionServiceBase<PodcastEntry> {
-    constructor(entityCollectionServiceFactory: EntityCollectionServiceFactory) {
+    constructor(entityCollectionServiceFactory: EntityCollectionServiceElementsFactory) {
         super('Entry', entityCollectionServiceFactory);
     }
 }
