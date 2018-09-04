3c67d676170c9aaddfd54f31b1b715767c8eb208
Fergal Moran <fergal.moran@gmail.com>
2018-09-04 01:32:11 +0100

    FUCK

--- src/app/shared/validators/conditional.validator.ts
+++ src/app/shared/validators/conditional.validator.ts
@@ -1,40 +1,45 @@
-export class ConditionalValidator {
-    static conditional(conditional, validator) {
-        return function(control) {
-            revalidateOnChanges(control);
+// import { distinctUntilChanged } from 'rxjs/operators';
+// import { FormControl } from '@angular/forms';
 
-            if (control && control._parent) {
-                if (conditional(control._parent)) {
-                    return validator(control);
-                }
-            }
-        };
-    }
-}
-function revalidateOnChanges(control): void {
-    if (control && control._parent && !control._revalidateOnChanges) {
-        control._revalidateOnChanges = true;
-        control._parent.valueChanges
-            .distinctUntilChanged((a, b) => {
-                // These will always be plain objects coming from the form, do a simple comparison
-                if ((a && !b) || (!a && b)) {
-                    return false;
-                } else if (a && b && Object.keys(a).length !== Object.keys(b).length) {
-                    return false;
-                } else if (a && b) {
-                    for (let i in a) {
-                        if (a[i] !== b[i]) {
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            })
-            .subscribe(() => {
-                control.updateValueAndValidity();
-            });
+// export class ConditionalValidator {
+//     static conditional(conditional, validator) {
+//         return function(control) {
+//             revalidateOnChanges(control);
 
-        control.updateValueAndValidity();
-    }
-    return;
-}
+//             if (control && control._parent) {
+//                 if (conditional(control._parent)) {
+//                     return validator(control);
+//                 }
+//             }
+//         };
+//     }
+// }
+// function revalidateOnChanges(control: FormControl): void {
+//     if (control && control._parent && !control._revalidateOnChanges) {
+//         control._revalidateOnChanges = true;
+//         control._parent.valueChanges
+//             .map(
+//                 distinctUntilChanged((a, b) => {
+//                     // These will always be plain objects coming from the form, do a simple comparison
+//                     if ((a && !b) || (!a && b)) {
+//                         return false;
+//                     } else if (a && b && Object.keys(a).length !== Object.keys(b).length) {
+//                         return false;
+//                     } else if (a && b) {
+//                         for (const i in a) {
+//                             if (a[i] !== b[i]) {
+//                                 return false;
+//                             }
+//                         }
+//                     }
+//                     return true;
+//                 })
+//             )
+//             .subscribe(() => {
+//                 control.updateValueAndValidity();
+//             });
+
+//         control.updateValueAndValidity();
+//     }
+//     return;
+// }
