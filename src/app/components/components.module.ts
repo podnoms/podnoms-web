8352dba2dfc79868f0bd969be423a6b72ca94c39
Fergal Moran <fergal.moran@gmail.com>
2019-01-15 12:17:23 +0000

    Remove unneeded scroll

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -16,6 +16,7 @@ import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+    suppressScrollX: true
 };
 @NgModule({
     imports: [CommonModule, RouterModule, SharedModule, ChartsModule, PerfectScrollbarModule, NgbTooltipModule],
