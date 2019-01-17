2762e5f87e1a94636c99d98e9961a1ad3a160d52
Fergal Moran <fergal.moran@gmail.com>
2019-01-17 16:39:46 +0000

    Nothing much

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
     production: true,
-    apiHost: 'https://api.podnoms.com',
-    signalRHost: 'https://rt.podnoms.com',
+    // apiHost: 'https://api.podnoms.com',
+    // signalRHost: 'https://rt.podnoms.com',
+    signalRHost: 'http://localhost:5000',
+    apiHost: 'http://localhost:5000',
     vapidPublicKey: 'BNfw7YvE15a7JdmladYN0Gx5U8y2x0kZyLswuXLemz8dzU36ssI-pRbTEfNct2TzQC5K4KU5raPcV1PkYeMjtz4',
     helpUrl: 'https://talk.podnoms.com/',
     version: require('../../package.json').version,
