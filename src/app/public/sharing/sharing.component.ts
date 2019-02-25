f7ec2baf844ebda65e464e26f47ac2610d6a7af4
Fergal Moran <fergal.moran@gmail.com>
2019-02-25 18:33:39 +0000

    Pre-merging

--- src/app/public/sharing/sharing.component.ts
+++ src/app/public/sharing/sharing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { SharingService } from '../../shared/services/sharing.service';
 import { ActivatedRoute } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-sharing',
@@ -12,12 +13,16 @@ export class SharingComponent implements OnInit, OnDestroy {
     sharedItem$: Observable<any>;
     sub: any;
     sharingLink: string;
-    constructor(private sharingService: SharingService, private route: ActivatedRoute) {}
+    constructor(private sharingService: SharingService, private route: ActivatedRoute, private meta: Meta) {}
 
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
             this.sharingLink = params['id'];
             this.sharedItem$ = this.sharingService.getSharingItem(this.sharingLink);
+
+            this.sharedItem$.subscribe(i => {
+                this.meta.addTags([{ name: 'og:image', content: i.imageUrl }]);
+            });
         });
     }
     ngOnDestroy() {
