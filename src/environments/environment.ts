3377333a722a9610adf4adb0948734e2673bb898
Fergal Moran <fergal.moran@gmail.com>
2019-05-24 19:51:00 +0100

    Package ujpdates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,14 @@ const emailRegex = {
 export const environment = {
     production: false,
     signalRHost: 'https://localhost:5001',
-    apiHost: 'https://localhost:5001',
-    vapidPublicKey: 'BNfw7YvE15a7JdmladYN0Gx5U8y2x0kZyLswuXLemz8dzU36ssI-pRbTEfNct2TzQC5K4KU5raPcV1PkYeMjtz4',
+    apiHost: 'http://localhost:5000',
+    vapidPublicKey:
+        'BNfw7YvE15a7JdmladYN0Gx5U8y2x0kZyLswuXLemz8dzU36ssI-pRbTEfNct2TzQC5K4KU5raPcV1PkYeMjtz4',
     helpUrl: 'https://talk.podnoms.com/',
     version: `${require('../../package.json').version}-debug`,
-    appInsightsConfig: { instrumentationKey: '020b002a-bd3d-4b25-8a74-cab16fd39dfc' },
+    appInsightsConfig: {
+        instrumentationKey: '020b002a-bd3d-4b25-8a74-cab16fd39dfc'
+    },
     dropboxAppKey: '1eovrmddgatke5a',
     firebase: {
         apiKey: 'AIzaSyA5pGl4o1oGJi1Ke-842Lq0VvL2YZU2rfc',
