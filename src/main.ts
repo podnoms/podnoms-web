95e50c510814901fe380dd6622dfbf24bb74580d
Fergal Moran <fergal.moran@gmail.com>
2019-06-09 18:56:36 +0100

    Disable the console

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,33 @@ import { AppModule } from './app/app.module';
 
 //#region console overwrite
 
-// if (environment.production && location.host.indexOf('localhost') !== 0 && false) {
-// enableProdMode();
-//     console.log(
-//         `%c ________________________________________
-// < mooooooooooooooooooooooooooooooooooooo >
-// <        Looking under the hood?         >
-// <  Join us: http://github.com/podnoms    >
-//  ----------------------------------------
-//         \\   ^__^
-//          \\  (oo)\\_______
-//             (__)\\       )\\/\\
-//                 ||----w |
-//                 ||     ||`,
-//         'font-family:monospace; color: brown; font-size: x-large'
-//     );
+if (
+    environment.production &&
+    location.host.indexOf('localhost') !== 0 &&
+    false
+) {
+    enableProdMode();
+    console.log(
+        `%c ________________________________________
+< mooooooooooooooooooooooooooooooooooooo >
+<        Looking under the hood?         >
+<  Join us: http://github.com/podnoms    >
+ ----------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`,
+        'font-family:monospace; color: brown; font-size: x-large'
+    );
 
-//     if (typeof window.console !== 'undefined') {
-//         window.console.log = function() {};
-//         window.console.debug = function() {};
-//         window.console.warn = function() {};
-//         window.console.error = function() {};
-//     }
-// }
+    if (typeof window.console !== 'undefined') {
+        window.console.log = function() {};
+        window.console.debug = function() {};
+        window.console.warn = function() {};
+        window.console.error = function() {};
+    }
+}
 //#endregion
 if (environment.production) {
     enableProdMode();
