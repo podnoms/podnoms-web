d5045a65578836194fccf68d4f98d81e146f006a
Fergal Moran <fergal.moran@gmail.com>
2019-06-13 20:27:40 +0100

    Move scripts

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-npm --no-git-tag-version --tag-version-prefix="" version patch
-ng build --prod
-docker build -t podnoms.azurecr.io/podnoms.web .
-docker push podnoms.azurecr.io/podnoms.web
