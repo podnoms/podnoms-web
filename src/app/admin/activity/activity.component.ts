a8c9f798f3db6b48583ec2d2c3bf8c030126c238
Fergal Moran <fergal.moran@gmail.com>
2019-06-14 00:21:27 +0100

    Basic userlist working

--- src/app/admin/activity/activity.component.ts
+++ src/app/admin/activity/activity.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'environments/environment';
+import { Profile } from 'app/core';
 
 @Component({
-  selector: 'app-activity',
-  templateUrl: './activity.component.html',
-  styleUrls: ['./activity.component.scss']
+    selector: 'app-activity',
+    templateUrl: './activity.component.html',
+    styleUrls: ['./activity.component.scss']
 })
 export class ActivityComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    users$: Observable<Profile>;
+    constructor(private http: HttpClient) {
+        this.users$ = http.get<Profile>(`${environment.apiHost}/admin/user`);
+    }
+    ngOnInit() {}
 }
