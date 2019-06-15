70dfbf455f009426b91eb247fe62a38549289a87
Fergal Moran <fergal.moran@gmail.com>
2019-06-15 06:17:23 +0100

    Tidy up grid

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ActivityComponent } from './activity/activity.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { MomentModule } from 'ngx-moment';
 
 @NgModule({
     declarations: [ActivityComponent],
-    imports: [CommonModule, AdminRoutingModule, NgxDatatableModule]
+    imports: [
+        CommonModule,
+        AdminRoutingModule,
+        MomentModule,
+        NgxDatatableModule
+    ]
 })
 export class AdminModule {}
