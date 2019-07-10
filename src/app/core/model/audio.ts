a910efd37a9cb2a847b36e5b2c8af47d047132f1
Fergal Moran <fergal.moran@gmail.com>
2019-07-10 19:52:10 +0100

    Initial realtime updating kinda working

--- src/app/core/model/audio.ts
+++ src/app/core/model/audio.ts
@@ -1,6 +1,30 @@
 export class AudioProcessingMessage {
+    processingStatus: number;
+    payload: any;
+    /*
     percentage: number;
     totalSize: string;
     currentSpeed: string;
     eTA: string;
+    */
+    static getProcessingStatus(status: number) {
+        switch (status) {
+            case 0:
+                return 'Accepted';
+            case 1:
+                return 'Processing';
+            case 2:
+                return 'Downloading';
+            case 3:
+                return 'Converting';
+            case 4:
+                return 'Uploading';
+            case 5:
+                return 'Processed';
+            case 6:
+                return 'Failed';
+            case 7:
+                return 'Deferred';
+        }
+    }
 }
