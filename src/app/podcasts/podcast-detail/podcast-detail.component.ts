e580336994ebfae816be7fa3933fb9950b8ba0ee
Fergal Moran <fergal.moran@gmail.com>
2019-08-22 19:10:02 +0100

    Fix issue with entries not loading post-edit

--- src/app/podcasts/podcast-detail/podcast-detail.component.ts
+++ src/app/podcasts/podcast-detail/podcast-detail.component.ts
@@ -1,9 +1,22 @@
-import { Component, Input, ChangeDetectionStrategy, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import {
+    Component,
+    Input,
+    ChangeDetectionStrategy,
+    OnInit,
+    OnChanges,
+    SimpleChanges
+} from '@angular/core';
 
 import { Podcast, PodcastEntry } from '../../core';
 import { PodcastStoreService } from '../podcast-store.service';
 import { PodcastDataService } from '../podcast-data.service';
-import { trigger, transition, style, sequence, animate } from '@angular/animations';
+import {
+    trigger,
+    transition,
+    style,
+    sequence,
+    animate
+} from '@angular/animations';
 import { Observable } from 'rxjs';
 import { EntriesStoreService } from '../entries-store.service';
 import { EntryDataService } from '../entry-data.service';
@@ -17,8 +30,14 @@ import { DragDropService } from '../../shared/services/drag-drop.service';
     changeDetection: ChangeDetectionStrategy.OnPush,
     animations: [
         trigger('fade', [
-            transition('void => *', [style({ opacity: '0' }), animate('1s ease-out', style({ opacity: '1' }))]),
-            transition('* => void', [style({ opacity: '1' }), animate('.2s ease-in', style({ opacity: '0' }))])
+            transition('void => *', [
+                style({ opacity: '0' }),
+                animate('1s ease-out', style({ opacity: '1' }))
+            ]),
+            transition('* => void', [
+                style({ opacity: '1' }),
+                animate('.2s ease-in', style({ opacity: '0' }))
+            ])
         ])
     ]
 })
@@ -35,20 +54,33 @@ export class PodcastDetailComponent implements OnInit, OnChanges {
     ) {}
 
     ngOnInit() {
-        this.entries$ = this.entriesStore.getWithQuery({ podcastSlug: this.podcast.slug });
+        this.entries$ = this.entriesStore.getWithQuery({
+            podcastSlug: this.podcast.slug
+        });
+        this.entries$.subscribe(p =>
+            console.log('podcast-detail.component', 'entries', p)
+        );
     }
     ngOnChanges(changes: SimpleChanges) {
         if (changes.podcast) {
-            this.entries$ = this.entriesStore.getWithQuery({ podcastSlug: changes.podcast.currentValue.slug });
+            this.entries$ = this.entriesStore.getWithQuery({
+                podcastSlug: changes.podcast.currentValue.slug
+            });
         }
     }
     deleteEntry(entry: PodcastEntry) {
         this.podcastEntryDataService.deleteEntry(entry.id).subscribe(
             () => {
-                this.podcast.podcastEntries = this.podcast.podcastEntries.filter(obj => obj.id !== entry.id);
+                this.podcast.podcastEntries = this.podcast.podcastEntries.filter(
+                    obj => obj.id !== entry.id
+                );
                 this.podcastStore.updateOneInCache(this.podcast);
             },
-            () => this.alertService.error('Error deleting entry', 'Please refresh page and try again')
+            () =>
+                this.alertService.error(
+                    'Error deleting entry',
+                    'Please refresh page and try again'
+                )
         );
     }
     dragStart($event: DragEvent, entry: PodcastEntry) {
