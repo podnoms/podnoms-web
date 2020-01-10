c4e302cff3b8b9fa93a30015072167aacf730576
Fergal Moran <fergal.moran@gmail.com>
2020-01-10 11:14:26 +0000

    Basic playing stuff done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,8 @@ export const environment = {
     production: false,
     signalRHost: 'https://podnoms.local:5001',
     apiHost: 'https://podnoms.local:5001',
+    radioHost: 'http://localhost:8000',
+    radioMount: 'podnoms',
     vapidPublicKey:
         'BNfw7YvE15a7JdmladYN0Gx5U8y2x0kZyLswuXLemz8dzU36ssI-pRbTEfNct2TzQC5K4KU5raPcV1PkYeMjtz4',
     helpUrl: 'https://talk.podnoms.com/',
