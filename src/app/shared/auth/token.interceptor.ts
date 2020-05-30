1ae7324374e6b0cdeb1ad9ae9d025af4e2abe13f
Fergal Moran <fergal.moran@gmail.com>
2020-05-30 09:14:01 +0100

    Tokens refreshing

--- src/app/shared/auth/token.interceptor.ts
+++ src/app/shared/auth/token.interceptor.ts
@@ -4,45 +4,98 @@ import {
     HttpInterceptor,
     HttpHandler,
     HttpRequest,
-    HttpHeaders
+    HttpHeaders,
+    HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, throwError } from 'rxjs';
 import { AuthService } from '../../auth/auth.service';
 import { environment } from 'environments/environment';
+import { switchMap, filter, take, catchError } from 'rxjs/operators';
+import { NGXLogger } from 'ngx-logger';
+import { auth } from 'firebase';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) {}
+    private isRefreshing = false;
+    private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<
+        any
+    >(null);
+
+    constructor(private authService: AuthService, private logger: NGXLogger) {}
     private commonHeaders(): HttpHeaders {
         const headers = new HttpHeaders({
             'cache-control': 'no-cache',
-            'content-type': 'application/json'
+            'content-type': 'application/json',
         });
         return headers;
     }
 
     intercept(
-        req: HttpRequest<any>,
+        request: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
         // TODO: Bigtime revisit this
         // 1. We don't want to be passing JWT tokens to random sites
         // 2. We probably don't want to be calling random sites from our front end?
         // --- all requests should go through our API.
-
-        if (!this.authService || !req.url.startsWith(environment.apiHost)) {
-            return next.handle(req);
-        }
         const authToken = this.authService.getAuthToken();
-        let headers =
-            req.url.indexOf('imageupload') > -1
+
+        if (
+            !this.authService ||
+            !request.url.startsWith(environment.apiHost) ||
+            !authToken
+        ) {
+            return next.handle(request);
+        }
+        const headers =
+            request.url.indexOf('imageupload') > -1
                 ? new HttpHeaders()
                 : this.commonHeaders();
-        if (authToken) {
-            headers = headers.append('Authorization', `Bearer ${authToken}`);
-        }
-        const changedReq = req.clone({
-            headers: headers
+
+        request = this.addToken(request, authToken);
+
+        return next.handle(request).pipe(
+            catchError((error) => {
+                if (
+                    error instanceof HttpErrorResponse &&
+                    error.status === 401
+                ) {
+                    return this.handle401Error(request, next);
+                } else {
+                    return throwError(error);
+                }
+            })
+        );
+    }
+
+    private addToken(request: HttpRequest<any>, token: string) {
+        return request.clone({
+            setHeaders: {
+                Authorization: `Bearer ${token}`,
+            },
         });
-        return next.handle(changedReq);
+    }
+
+    private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+        if (!this.isRefreshing) {
+            this.isRefreshing = true;
+            this.refreshTokenSubject.next(null);
+
+            return this.authService.refreshToken().pipe(
+                switchMap((token: any) => {
+                    this.isRefreshing = false;
+                    this.refreshTokenSubject.next(token);
+                    return next.handle(this.addToken(request, token));
+                })
+            );
+        } else {
+            return this.refreshTokenSubject.pipe(
+                filter((token) => token != null),
+                take(1),
+                switchMap((jwt) => {
+                    this.isRefreshing = false;
+                    return next.handle(this.addToken(request, jwt));
+                })
+            );
+        }
     }
 }
