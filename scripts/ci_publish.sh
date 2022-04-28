c4b905de7bb9996dec8f9822ebb8c10bad64cc20
Fergal Moran <fergal.moran@gmail.com>
2022-04-28 02:21:37 +0100

    Doing a pub

--- scripts/ci_publish.sh
+++ scripts/ci_publish.sh
@@ -1,12 +1,12 @@
-#1 /usr/bin/env bash
+#!/usr/bin/env bash
 
 if [ -z "$(git status --porcelain)" ]; then
-    echo 'Good to go'
-    git checkout master && \
-        git merge develop && \
-        git push origin trunk develop && \
-        git checkout develop
+  echo 'Good to go'
+  git checkout master && \
+  git merge develop && \
+  git push origin trunk develop && \
+  git checkout develop
 else
-    echo 'Please commit all changes first'
-    git status --porcelain
+  echo 'Please commit all changes first'
+  git status --porcelain
 fi
