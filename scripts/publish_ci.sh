2f1746751e29bfa5d1e312d31e83be8f07d110f8
Fergal Moran <fergal.moran@gmail.com>
2022-07-13 11:37:44 +0100

    Fix publish script

--- scripts/publish_ci.sh
+++ scripts/publish_ci.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+patchlevel=patch
+
 if [ -z "$(git status --porcelain)" ]; then
   echo 'Good to go'
-  git checkout master && \
-  git merge develop && \
-  git push origin trunk develop && \
-  git checkout develop
+  npm --no-git-tag-version --tag-version-prefix="" version $patchlevel
+  git commit -am "Releasing patch"
+  git checkout trunk &&
+    git merge develop &&
+    git push origin trunk develop &&
+    git checkout develop
 else
   echo 'Please commit all changes first'
   git status --porcelain
