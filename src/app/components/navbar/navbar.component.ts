8393b8cb427692f5b8b5268c2f30dc2060c5862d
Fergal Moran <fergal.moran@gmail.com>
2022-07-13 11:32:22 +0100

    Add back support link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,72 +13,71 @@ import { ProfileStoreService } from '../../profile/profile-store.service';
 import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
-    animations: [
-        trigger('inOutAnimation', [
-            transition(':enter', [
-                style({ height: 0, opacity: 0 }),
-                animate('1s ease-out', style({ height: 300, opacity: 1 })),
-            ]),
-            transition(':leave', [
-                style({ height: 300, opacity: 1 }),
-                animate('1s ease-in', style({ height: 0, opacity: 0 })),
-            ]),
-        ]),
-    ],
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [
+        style({ height: 0, opacity: 0 }),
+        animate('1s ease-out', style({ height: 300, opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ height: 300, opacity: 1 }),
+        animate('1s ease-in', style({ height: 0, opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class NavbarComponent {
-    profile$: Observable<Profile>;
-    invoices$: Observable<Payment[]>;
-    pricingEnabled: boolean = false;
-    profileHasAdmin: boolean = false;
-    searchActive: boolean = false;
-    constructor(
-        paymentService: PaymentsService,
-        private authService: AuthService,
-        private debugService: DebugService,
-        private alertService: AlertService,
-        private profileStoreService: ProfileStoreService,
-        protected logger: NGXLogger,
-        protected uiStateService: UiStateService
-    ) {
-        this.profile$ = this.profileStoreService.entities$.pipe(
-            filter((r) => r !== null && r !== []),
-            map((r) => {
-                this.logger.debug('navbar.component', 'profilePipe', r);
-                return r[0];
-            })
-        );
-        this.profile$.subscribe((p) => {
-            this.profileHasAdmin = this.authService.checkHasRoles([
-                'website-admin',
-            ]);
-        });
-        this.invoices$ = paymentService.getPayments();
-    }
-    toggleSidebar() {
-        this.uiStateService.toggleSidebar();
-    }
-    toggleOverlay() {
-        this.uiStateService.toggleOverlay();
-    }
-    logout() {
-        this.authService.logout();
-    }
-    about() {
-        this.debugService.getDebugInfo().subscribe((r) => {
-            this.logger.debug('navbar.component', 'about', r);
-            this.alertService.info(
-                'About',
-                `Client Version: ${environment.version}<br />` +
-                    `API Version: ${r['version']}<br />` +
-                    `Host: ${r['osVersion']['versionString']}`,
-                {
-                    autoClose: false,
-                }
-            );
-        });
-    }
+  profile$: Observable<Profile>;
+  invoices$: Observable<Payment[]>;
+  pricingEnabled: boolean = false;
+  profileHasAdmin: boolean = false;
+  searchActive: boolean = false;
+  helpUrl: string = environment.helpUrl;
+  constructor(
+    paymentService: PaymentsService,
+    private authService: AuthService,
+    private debugService: DebugService,
+    private alertService: AlertService,
+    private profileStoreService: ProfileStoreService,
+    protected logger: NGXLogger,
+    protected uiStateService: UiStateService
+  ) {
+    this.profile$ = this.profileStoreService.entities$.pipe(
+      filter((r) => r !== null && r !== []),
+      map((r) => {
+        this.logger.debug('navbar.component', 'profilePipe', r);
+        return r[0];
+      })
+    );
+    this.profile$.subscribe((p) => {
+      this.profileHasAdmin = this.authService.checkHasRoles(['website-admin']);
+    });
+    this.invoices$ = paymentService.getPayments();
+  }
+  toggleSidebar() {
+    this.uiStateService.toggleSidebar();
+  }
+  toggleOverlay() {
+    this.uiStateService.toggleOverlay();
+  }
+  logout() {
+    this.authService.logout();
+  }
+  about() {
+    this.debugService.getDebugInfo().subscribe((r) => {
+      this.logger.debug('navbar.component', 'about', r);
+      this.alertService.info(
+        'About',
+        `Client Version: ${environment.version}<br />` +
+          `API Version: ${r['version']}<br />` +
+          `Host: ${r['osVersion']['versionString']}`,
+        {
+          autoClose: false,
+        }
+      );
+    });
+  }
 }
--- src/app/payments/make-payment/make-payment.component.ts
+++ src/app/payments/make-payment/make-payment.component.ts
@@ -1,8 +1,8 @@
 import {
-    ChangeDetectorRef,
-    Component,
-    HostListener,
-    OnInit
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  OnInit,
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NGXLogger } from 'ngx-logger';
@@ -14,131 +14,115 @@ import { PaymentsService } from '../payments.service';
 declare var StripeCheckout: any;
 
 @Component({
-    selector: 'app-make-payment',
-    templateUrl: './make-payment.component.html',
-    styleUrls: ['./make-payment.component.scss'],
+  selector: 'app-make-payment',
+  templateUrl: './make-payment.component.html',
+  styleUrls: ['./make-payment.component.scss'],
 })
 export class MakePaymentComponent implements OnInit {
-    loadingText: string = 'Loading payment methods';
-    errorText: string = '';
-    headerText: string = '';
-    handler: any;
-    error: string;
-    tier: any;
-    chargingAmount: number = -1;
-    constructor(
-        private cd: ChangeDetectorRef,
-        private router: Router,
-        private route: ActivatedRoute,
-        private authService: AuthService,
-        private scriptService: ScriptService,
-        private paymentService: PaymentsService,
-        private alertService: AlertService,
-        protected logger: NGXLogger
-    ) {}
-    ngOnInit() {
-        const type = this.route.snapshot.params.type || 'advanced';
-        switch (type) {
-            case 'Free':
-                this.headerText =
-                    'Buy me a coffee, or a beer, or an Aston Martin';
-                break;
-            case 'Personal':
-                this.headerText = 'Purchase 1 month advanced subscription';
-                break;
-            case 'Professional':
-                this.headerText = 'Purchase 1 month professional subscription';
-                break;
-        }
+  loadingText: string = 'Loading payment methods';
+  errorText: string = '';
+  headerText: string = '';
+  handler: any;
+  error: string;
+  tier: any;
+  chargingAmount: number = -1;
+  constructor(
+    private cd: ChangeDetectorRef,
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private scriptService: ScriptService,
+    private paymentService: PaymentsService,
+    private alertService: AlertService,
+    protected logger: NGXLogger
+  ) {}
+  ngOnInit() {
+    const type = this.route.snapshot.params.type || 'advanced';
+    switch (type) {
+      case 'Free':
+        this.headerText = 'Buy me a coffee, or a beer, or an Aston Martin';
+        break;
+      case 'Personal':
+        this.headerText = 'Purchase 1 month advanced subscription';
+        break;
+      case 'Professional':
+        this.headerText = 'Purchase 1 month professional subscription';
+        break;
+    }
 
-        this.paymentService.getPricingTier(type).subscribe(
-            (p) => {
-                this.tier = p;
-                this._spinUpMerchant(p);
-            },
-            (err) => {
-                this.logger.debug(
-                    'make-payment.component',
-                    'getPricingTier',
-                    err
-                );
-                this.errorText =
-                    'Unable to load pricing tier, please try again later';
-            }
-        );
-    }
-    _spinUpMerchant(pricingTier: any) {
-        this.scriptService
-            .load('stripe')
-            .then(() => {
-                this.handler = StripeCheckout.configure({
-                    key: environment.stripeKey,
-                    image: 'https://www.podnoms.com/assets/img/logo-icon.png',
-                    locale: 'auto',
-                    currency: 'EUR',
-                    token: (token: { id: any }) => {
-                        this.loadingText = 'Processing payment';
-                        this.paymentService
-                            .processPayment(
-                                token.id,
-                                this.chargingAmount,
-                                pricingTier.type
-                            )
-                            .subscribe(
-                                (r) => {
-                                    if (r) {
-                                        this.authService
-                                            .reloadProfile()
-                                            .subscribe(() => {
-                                                this.alertService.success(
-                                                    'Success',
-                                                    pricingTier.type === 'Free'
-                                                        ? 'THANK YOU SO MUCH!!!!'
-                                                        : 'Payment successfully received.'
-                                                );
-                                                this.router.navigate(['']);
-                                            });
-                                    }
-                                },
-                                (error) => {
-                                    this.loadingText = '';
-                                    this.errorText =
-                                        'There was an error processing your payment, ' +
-                                        'please open a ticket at ' +
-                                        '<a href="https://talk.podnoms.com/"> PodNoms Support</a> so we can track your payment';
-                                }
-                            );
-                    },
-                });
-                this.loadingText = '';
-            })
-            .catch((err) =>
-                this.logger.error(
-                    'make-payment.component',
-                    'Error loading stripe',
-                    err
-                )
-            );
-    }
-    handlePayment() {
-        this.chargingAmount = this.tier.costPerMonth;
-        this.handler.open({
-            name: 'PodNoms',
-            description: this.tier.title,
-            amount: this.chargingAmount,
+    this.paymentService.getPricingTier(type).subscribe(
+      (p) => {
+        this.tier = p;
+        this._spinUpMerchant(p);
+      },
+      (err) => {
+        this.logger.debug('make-payment.component', 'getPricingTier', err);
+        this.errorText = 'Unable to load pricing tier, please try again later';
+      }
+    );
+  }
+  _spinUpMerchant(pricingTier: any) {
+    this.scriptService
+      .load('stripe')
+      .then(() => {
+        this.handler = StripeCheckout.configure({
+          key: environment.stripeKey,
+          image: 'https://www.podnoms.com/assets/img/logo-icon.png',
+          locale: 'auto',
+          currency: 'EUR',
+          token: (token: { id: any }) => {
+            this.loadingText = 'Processing payment';
+            this.paymentService
+              .processPayment(token.id, this.chargingAmount, pricingTier.type)
+              .subscribe(
+                (r) => {
+                  if (r) {
+                    this.authService.reloadProfile().subscribe(() => {
+                      this.alertService.success(
+                        'Success',
+                        pricingTier.type === 'Free'
+                          ? 'THANK YOU SO MUCH!!!!'
+                          : 'Payment successfully received.'
+                      );
+                      this.router.navigate(['']);
+                    });
+                  }
+                },
+                (error) => {
+                  this.loadingText = '';
+                  this.errorText =
+                    'There was an error processing your payment, ' +
+                    'please open a ticket at ' +
+                    `<a href="${environment.helpUrl}"> PodNoms Support</a> so we can track your payment`;
+                }
+              );
+          },
         });
-    }
-    handleDonation(amount: any) {
-        this.chargingAmount = amount * 100;
-        this.logger.debug('make-payment.component', 'handleDonation', amount);
-        this.handler.open({
-            name: 'PodNoms',
-            description: this.tier.title,
-            amount: this.chargingAmount,
-        });
-    }
-    @HostListener('window:popstate')
-    onpopstate() {
-        this.handler.close();
-    }
+        this.loadingText = '';
+      })
+      .catch((err) =>
+        this.logger.error('make-payment.component', 'Error loading stripe', err)
+      );
+  }
+  handlePayment() {
+    this.chargingAmount = this.tier.costPerMonth;
+    this.handler.open({
+      name: 'PodNoms',
+      description: this.tier.title,
+      amount: this.chargingAmount,
+    });
+  }
+  handleDonation(amount: any) {
+    this.chargingAmount = amount * 100;
+    this.logger.debug('make-payment.component', 'handleDonation', amount);
+    this.handler.open({
+      name: 'PodNoms',
+      description: this.tier.title,
+      amount: this.chargingAmount,
+    });
+  }
+  @HostListener('window:popstate')
+  onpopstate() {
+    this.handler.close();
+  }
 }
