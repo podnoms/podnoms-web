d8cd7a5cfd670da4c72bac35f67081c17a5be049
Fergal Moran <fergal.moran@gmail.com>
2025-07-21 20:48:37 +0100

    Updated ng update @angular/cli

--- src/app/components/shared/rediroll/rediroll.component.ts
+++ src/app/components/shared/rediroll/rediroll.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component, OnInit, Inject, DOCUMENT } from '@angular/core';
+
 
 @Component({
     selector: 'app-rediroll',
